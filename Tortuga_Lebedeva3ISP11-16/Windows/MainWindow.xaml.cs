4fad8ce62528010641ca288573151d966225f1cb
Sergey Panchenko <forelock10@gmail.com>
2022-10-22 10:39:59 +0300

    Circles1

--- Tortuga_Lebedeva3ISP11-16/Windows/MainWindow.xaml.cs
+++ Tortuga_Lebedeva3ISP11-16/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tortuga_Lebedeva3ISP11_16.Windows;
 
 namespace Tortuga_Lebedeva3ISP11_16
 {
@@ -24,6 +25,174 @@ namespace Tortuga_Lebedeva3ISP11_16
         {
             InitializeComponent();
             
+        }
+
+        private void Btn_4_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_5_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_6_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_7_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_8_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_9_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_10_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_11_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_12_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn23_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn24_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_13_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_14_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_3_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_2_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_19_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_20_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_21_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_22_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_15_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_16_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_17_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_18_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMenu menuWindow = new WindowMenu();
+            menuWindow.Show();
+            this.Close();
         }
     }
 }
